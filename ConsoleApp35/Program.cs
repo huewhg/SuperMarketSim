daea0048a39e03bb54f6c483a7e05538abebab85
Huewhg <adam.dragon.karasek@gmail.com>
2025-12-16 17:34:27 +0100

    Init

--- ConsoleApp35/Program.cs
+++ ConsoleApp35/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int arriveInterval;
+        int simLength;
+        DateTime startTime;
+        DateTime endTime;
+        int maxNew;
+        int minNew;
+        int numRegisters;
+        int timePerItem;
+        int maxItems;
+        int leaveProbability;
     }
 }
